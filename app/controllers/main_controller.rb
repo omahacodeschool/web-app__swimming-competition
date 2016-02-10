6054ab2e6c26bd0aa865e469f8263a188c8676a2
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-10 12:02:18 -0600

    First 2 controller actions and views added.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+MyApp.get "/events" do
+  erb :"main/events"
+end
+
+MyApp.get "/styles" do
+  erb :"main/styles"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,18 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "gender"
+    t.string   "distance"
+    t.integer  "style id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "style"
+  end
+
 end
