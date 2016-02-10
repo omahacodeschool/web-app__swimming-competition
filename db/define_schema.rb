3ca6ad8f7fee5a864388f34caa01b3135576601d
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-10 14:43:10 -0600

    added handful of empty views & controllers

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -10,3 +10,15 @@ DB.define_column("competitors", "college_id", "integer")
 DB.define_table("activities")
 DB.define_column("activities", "activity_name", "string")
 DB.define_column("activities", "max_score", "integer")
+
+DB.define_table("results")
+DB.define_column("results", "student_id", "integer")
+DB.define_column("results", "activity_id", "integer")
+DB.define_column("results", "student_score", "integer")
+
+DB.define_table("colleges")
+DB.define_column("colleges", "college_name", "string")
+DB.define_column("colleges", "conference_id", "integer")
+
+DB.define_table("conferences")
+DB.define_column("conferences", "conference_name", "string")
