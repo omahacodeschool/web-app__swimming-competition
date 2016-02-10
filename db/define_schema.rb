7bcd362964cf7758c504daa730effe0deb44e816
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-10 14:32:01 -0600

    define_schema && models set up

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -1,14 +1,12 @@
 require_relative "./_configure"
 
-# Define your tables and their columns below this line.
-# -----------------------------------------------------------------------------
+DB.define_table("competitors")
+DB.define_column("competitors", "first_name", "string")
+DB.define_column("competitors", "last_name", "string")
+DB.define_column("competitors", "result", "string")
+DB.define_column("competitors", "age", "integer")
+DB.define_column("competitors", "college_id", "integer")
 
-# Column names should be all lowercase and have no spaces or any other special
-# characters--except for underscores. So "running_time" is valid, but
-# "running time" and "running_time!" are not vaild.
-
-# The `id` column and timestamp columns are automatically added.
-# Do not define those columns yourself.
-
-# What table do you need to store the necessary information?
-# What columns do you need in that table?
+DB.define_table("activities")
+DB.define_column("activities", "activity_name", "string")
+DB.define_column("activities", "max_score", "integer")
