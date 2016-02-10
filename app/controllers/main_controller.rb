507e4b99d509bed70fb2aedcdbd3eba21330fd25
Michael Meier <mmeier61@yahoo.com>
2016-02-10 14:06:56 -0600

    updated

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -19,3 +19,24 @@ MyApp.get "/administrator" do
 
   erb :"administrator"
 end
+MyApp.get "/add_swimmer" do
+  swimer_add = Swimmer.new
+  swimmer_add.name = params[:new_swimmer]
+  swimmer_add.save
+  allswimmers = Swimer.all
+  erb :"administrator"
+end
+MyApp.get "/add_event" do
+  event_add = Event.new
+  event_add.event_name = params[:new_event]
+  event_add.save
+  allevents = Event.all
+  erb :"administrator"
+end
+MyApp.get "/add_school" do
+  school_add = School.new
+  school_add.school_name = params[:new_school]
+  school_add.save
+  allschools = School.all
+  erb :"administrator"
+end
