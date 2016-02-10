76ec653adedc623f6e6697e42624921a721c025b
Kyle Sandage <ksandage08@gmail.com>
2016-02-10 14:55:02 -0600

    Created what I will call 'flow' controllers and... created views that display forms for adding information. Also created views that display a list of one particular set of information in the database. ie. one view would display a list of the universities that have been added.

---
+++ app/controllers/flow_controllers.rb
@@ -0,0 +1,63 @@
+MyApp.get "/" do
+  erb :"homepage"
+end
+
+# Go to a view with a form for adding swimmers.
+MyApp.get "/new_swimmer" do
+  erb :"create/create_swimmer"
+end
+
+# Go to a view that only displays the current swimmers.
+MyApp.get "/show_swimmers" do
+  @swimmers = Swimmer.all
+  erb :"display/display_swimmers"
+end
+
+# Go to a view with a form for adding conferences.
+MyApp.get "/new_conference" do
+  erb :"create/create_conference"
+end
+
+# Go to a view that only displays the current conferences.
+MyApp.get "/show_conferences" do
+  @conferences = Conference.all
+  erb :"display/display_conferences"
+end
+
+# Go to a view with a form for adding universities.
+MyApp.get "/new_university" do
+  erb :"create/create_university"
+end
+
+# Go to a view that only displays the current universities.
+MyApp.get "/show_universities" do
+  @universities = University.all
+  erb :"display/display_universities"
+end
+
+# Go to a view with a form for adding events.
+MyApp.get "/new_event" do
+  erb :"create/create_event"
+end
+
+# Go to a view that only displays the current list of events.
+MyApp.get "/show_events" do
+  @events = Event.all
+  erb :"display/display_events"
+end
+
+# Go to a view with a form for adding results.
+MyApp.get "/new_result" do
+  erb :"create/create_result"
+end
+
+# Go to a view that only displays the current list of results.
+MyApp.get "/show_results" do
+  @results = Result.all
+  erb :"display/display_results"
+end
+
+# Go to a view that only displays the current standings.
+MyApp.get "/show_standings" do
+  erb :"display/display_standings"
+end
