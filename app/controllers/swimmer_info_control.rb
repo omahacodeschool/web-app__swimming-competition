7a55aa9bcb23b4c2b3bf64a2ea21b5a3ca7d615a
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-10 17:57:59 -0600

    building swim info form. looking pretty ugly. might snatch one from the internet

--- app/controllers/swimmer_info_control.rb
+++ app/controllers/swimmer_info_control.rb
@@ -1,6 +1,12 @@
 MyApp.get "/swimmer_info_form" do
   @s = SwimmerInfo.new
+  @s.first_name = params["last_name"]
   @s.first_name = params["first_name"]
+  @s.first_name = params["team_id"]
+  @s.first_name = params["age"]
+  @s.first_name = params["gender_id"]
+  @s.first_name = params["height"]
+  @s.first_name = params["weight"]
   @s.save
   erb :"/swimmer/swimmer_info_form"
 end
