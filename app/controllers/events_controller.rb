282cee3f74a85bb36c408f3d2fa02403982d9527
Chris Foral <chris.foral@gmail.com>
2016-02-10 14:48:13 -0600

    The Add Events page is working at the basic level that I've defined. Moving on to other controllers and views.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,8 +9,11 @@ MyApp.get "/add_events" do
 end
 #
 # Takes the name of each event submitted from the form on the add_events view
+# Want to use the final list of events to populate a checklist for an Add
+# Swimmers form
 #
 MyApp.get "/events_form" do
+  @events = Event.all
   @e = Event.new
   @e.event_name = (params["event_to_add"])
   @e.save
