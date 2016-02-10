157ed60cf5efe65e6bb915d737d8f53192178f66
Kyle Sandage <ksandage08@gmail.com>
2016-02-10 12:39:10 -0600

    All Tables Created (I think)

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -19,3 +19,26 @@ DB.define_column("swimmers", "university_id", "integer")
 
 DB.define_table("conferences")
 DB.define_column("conferences", "conference_name", "string")
+
+DB.define_table("universities")
+DB.define_column("universities", "university_name", "string")
+DB.define_column("universities", "conference_id", "integer")
+
+DB.define_table("events")
+DB.define_column("events", "event_name", "string")
+
+DB.define_table("results")
+DB.define_column("results", "swimmer_id", "integer")
+DB.define_column("results", "event_id", "integer")
+DB.define_column("results", "recorded_time", "string")
+
+DB.define_table("standings")
+DB.define_column("standings", "event_id", "integer")
+DB.define_column("standings", "first_id", "integer")
+DB.define_column("standings", "second_id", "integer")
+DB.define_column("standings", "third_id", "integer")
+
+#This last table has me a little confused at the moment.  In my mind I think that the "first_id" column will contain the id of the row from the "results" table with the fastest time for an event.  Describing this, my guess is that the challenge will be making sure that somehow a method finds the fastest times for ONLY the "event_id" being searched.  Otherwise it would just find the fastest time in the entire "results" table regardless of event.  
+
+
+
