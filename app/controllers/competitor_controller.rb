d803c78083e65bd8c076284f4ca0a32f2e42c133
Beth Haubert <bethany.haubert@gmail.com>
2016-02-10 14:10:25 -0600

    working dropdown in add competitor

--- app/controllers/competitor_controller.rb
+++ app/controllers/competitor_controller.rb
@@ -4,11 +4,11 @@ MyApp.get "/competitors" do
 end
 
 MyApp.get "/competitors/add_new" do
+		@list_schools = School.all
   erb :"competitors/add_competitor"
 end
 
 MyApp.get "/add_competitor" do
-	@list_schools = School.all
 	x = Competitor.new
 	x.first_name = params[:first_name]
 	x.last_name = params[:last_name]
