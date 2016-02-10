99bd05f433eb89eeeae8b37a36e15ea2f75dd892
Beth Haubert <bethany.haubert@gmail.com>
2016-02-09 20:56:34 -0600

    added views, models, controllers

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -12,3 +12,27 @@ require_relative "./_configure"
 
 # What table do you need to store the necessary information?
 # What columns do you need in that table?
+
+DB.define_table("events")
+DB.define_column("events", "name", "string")
+DB.define_column("events", "first_place", "integer")
+DB.define_column("events", "second_place", "integer")
+DB.define_column("events", "third_place", "integer")
+
+DB.define_table("competitors")
+DB.define_column("competitors", "first_name", "string")
+DB.define_column("competitors", "last_name", "string")
+DB.define_column("competitors", "school_id", "integer")
+
+DB.define_table("schools")
+DB.define_column("schools", "school_name", "string")
+DB.define_column("schools", "conference_id", "integer")
+
+DB.define_table("conferences")
+DB.define_column("conferences", "name", "string")
+
+DB.define_table("results")
+DB.define_column("results", "event_id", "integer")
+DB.define_column("results", "competitor_id", "integer")
+DB.define_column("results", "time", "string")
+
--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,43 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "competitors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "school_id"
+  end
+
+  create_table "conferences", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "conference_name"
+    t.string   "name"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "name"
+    t.integer  "first_place"
+    t.integer  "second_place"
+    t.integer  "third_place"
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "event_id"
+    t.integer  "competitor_id"
+    t.string   "time"
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "school_name"
+    t.integer  "conference_id"
+  end
+
 end
