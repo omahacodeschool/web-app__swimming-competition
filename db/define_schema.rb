f19db8f2a8ead832ef509a3922eb1dd98bf4716b
Tray Harrington <traylon.harrington@gmail.com>
2016-02-10 23:11:02 -0600

    Set up tables

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -12,3 +12,29 @@ require_relative "./_configure"
 
 # What table do you need to store the necessary information?
 # What columns do you need in that table?
+
+#Set up Gamers table
+DB.define_table("gamers")
+DB.define_column("gamers", "last_name", "string")
+DB.define_column("gamers", "first_name", "string")
+DB.define_column("gamers", "squad_id", "integer")
+
+#Set up Games table
+DB.define_table("games")
+DB.define_column("games", "game_name")
+
+
+#Set up Squads table
+DB.define_table("squads")
+DB.define_column("squads", "squad_name", "string")
+DB.define_column("squads", "division_id", "integer")
+
+#Set up Divison table
+DB.define_table("divisions")
+DB.define_column("divisions", "division")
+
+#Set up Results table
+DB.define_table("results")
+DB.define_column("results", "game_id", "integer")
+DB.define_column("results", "gamer_id", "integer")
+DB.define_column("results", "completion_time", "integer")
