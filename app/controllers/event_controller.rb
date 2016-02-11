807c134e64aaeece499e5ad8bda0076e5f0f9aad
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-11 05:55:49 -0600

    added page to create a new event, added link to create a new event to hompepage

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,4 +1,8 @@
-MyApp.get "/add_event" do
+MyApp.get "/create_event" do
+  erb :"data_entry/event_new"
+end
+
+MyApp.get "/event_added" do
   x = Event.new
   x.name = params["event"]
   # x.event = params["event"]
