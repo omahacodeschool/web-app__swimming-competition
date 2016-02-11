5e57bbca412a6320f7e6b53f2ee066ca76d5ce2b
Ryan Newell <youcatastrophe@gmail.com>
2016-02-11 16:52:52 -0600

    Currently trying to get new_penguin.erb file to load without throwing a 'No method' error.

--- app/models/Penguin.rb
+++ app/models/Penguin.rb
@@ -3,7 +3,7 @@ class Penguin < ActiveRecord::Base
   # Get the Penguin's Rookery info
   #
   #Returns the Rookery object.
-  def rookery_acess
+  def rookery_access
     x = self.rookery_id
     y = Rookery.find_by_id(x)
     y
