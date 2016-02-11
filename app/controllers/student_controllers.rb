c104b6c540d241a7b988224a3152ab8f3e7a79df
Amy Wall <amymariewall@gmail.com>
2016-02-11 13:58:58 -0600

    Corresponding results are being deleted when a student gets deleted

--- app/controllers/student_controllers.rb
+++ app/controllers/student_controllers.rb
@@ -24,6 +24,8 @@ end
 MyApp.get "/delete/student/:student_id" do
   @student = Student.find_by_id(params[:student_id])
   @confirm_message = "Success! Deleted #{@student.first_name} #{@student.last_name}!"
+  @corresponding_results = Result.where({"student_id" => params[:student_id]})
+  @corresponding_results.delete_all
   @student.delete
   erb :"admin/confirm_submission"
 end
