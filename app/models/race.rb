ea6d40fd0ac56b21df212ace3bd6e24f74b0c6d1
Patrick Clark <patclark1958@gmail.com>
2016-02-12 12:20:06 -0600

    methods in race.rb and swim_entry.rb seem not even half-baked. Diff between use of .where and .find_by_something??

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,24 +4,14 @@ class Race < ActiveRecord:: Base
 
 #Race table has race ids, race names and start times.
 #SwimEntry table has race ids paired with swimmer ids for any instance of one swimmer entering one race. It does not have swimmer names, only ids. 
-#Swimmer has swimmer names (and other information) and swimmer ids.
+#Swimmer has swimmer names (and other information) and swimmer ids. No races.
 
-#It has race numbers and names and start times. SwimEntry has swimmer ids paired with race numbers of races the swimmer has entered. From the Race class I would need to define a method. Swimmers is the only table with swimmer names. 
+#So will I need another method in SwimEntry to return a hash of swimmer names and their swimmer ids? 
 
-#SwimEntry does not have swimmer names, just the ids. So will I need another method in SwimEntry to return a hash of swimmer names and their swimmer ids?  
-
-  def swimmers_ids_one_race
-    n = self.swimmer_id #swimmer id listed in the swim_entry table
-    s = SwimEntry.find_by_id(n) #method applied to class SwimEntry
-
-
-
-  end
-
-  def all_swimmer_names_one_race
-    r = self.id
-    n = SwimEntry.where({race_id => r}) #would return 
-
+  def swimmer_ids_one_race
+    r = self.id #
+    s = SwimEntry.where({race.id => r}) #(would that give me back the swimmer ids for all of the swimmers in one race?)
+    return s
   end
   
 
--- app/models/swim_entry.rb
+++ app/models/swim_entry.rb
@@ -1,2 +1,10 @@
 class SwimEntry < ActiveRecord:: Base
 end
+
+#maybe Class SwimEntry
+#This method should return the entire line of swimmer information for each swimmer who has one of the swimmer_ids tied to a particular race.
+  def swimmer_names_one_race
+    u = self.swimmer_id
+    y = Swimmer.find_by_name(u) #diff between ".where" and "find_by_something"?
+    return y #(Sumeet said it is ok to get the whole line of information)
+  end
