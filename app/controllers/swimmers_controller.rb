ed277cfafd44937c5548827ffc237bcd42a90d43
Chris Foral <chris.foral@gmail.com>
2016-02-12 14:11:47 -0600

    Still stuck on form/model method issue

--- app/controllers/swimmers_controller.rb
+++ app/controllers/swimmers_controller.rb
@@ -1,12 +1,13 @@
 MyApp.get "/swimmers_form" do
   @swimmers = Swimmer.all
-  
-  @s = Swimmer.new
-  @s.swimmer_name   = (params["swimmer_to_add"])
-  @s.swimmer_dob    = (params["swimmer_dob"])
-  @s.swimmer_gender = (params["swimmer_gender"])
-  @s.team_id        = (params["swimmer_team"])
-  @s.save
 
+  @s = Swimmer.new
+  @s.swimmer_name           = (params["swimmer_to_add"])
+  @s.swimmer_dob            = (params["swimmer_dob"])
+  @s.swimmer_gender         = (params["swimmer_gender"])
+  @s.team_id                = (params["swimmer_team"].to_i)
+  @s.swimmer_payment_status = (params["swimmer_payment_status"])
+  @s.save
+  
   erb :"main/add_swimmers"
 end
--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -1,6 +1,6 @@
 class Swimmer < ActiveRecord::Base
   @swimmers = Swimmer.all
-  
+
   def swimmer_team
     x = self.team_id
     y = Team.find_by_id(x)
