5d44ef6ada819c975af6bac971370214ca554b8a
Ryan Newell <youcatastrophe@gmail.com>
2016-02-12 12:39:48 -0600

    slight changes to edit_results form.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -18,5 +18,17 @@ end
 
 MyApp.get "/results/edit/:id" do
   @result = Result.find_by_id(params[:id])
+  @penguins = Penguin.all
+  @events = Event.all
   erb :"results/edit_result"
 end
+
+MyApp.post "/results/process_edit/:id"
+  @event = Event.find_by_id(params[:id])
+  @event.penguin_id = params["edit_penguin_id"]
+  @event.event_id = params["edit_event_id"]
+  @event.time = params["edit_time"]
+erb :"updated"
+end
+
+
