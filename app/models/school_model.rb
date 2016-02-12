fdec86b8a86b33e4a6ae99724f9a804f0a9f0839
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-12 13:40:56 -0600

    Got first conference in school list to show--need to find way to loop through all

--- app/models/school_model.rb
+++ app/models/school_model.rb
@@ -2,7 +2,11 @@ class School <ActiveRecord::Base
   def display_conference_of_school
     competitors = Competitor.all
     x = self.school_name
-    y = competitors.where({"school_name" => x})
-    return y
+    y = Competitor.where({"school_name" => x})
+    lololol = []
+    y.each do |y|
+      lololol << y.conference_name
+    end
+    return lololol[0]
   end
 end
