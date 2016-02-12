063cd27357c8387c16f24c384b25a23ea41680ea
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-11 22:47:17 -0600

    able to set age & school when adding a user. setting a school also subsequently correctly sets the conference

--- app/controllers/competitor_controller.rb
+++ app/controllers/competitor_controller.rb
@@ -1,11 +1,15 @@
 MyApp.get "/competitors/add_competitor" do
-
+  @colleges = College.all
   erb :"/competitors/add_competitor"
 end
 
 MyApp.get "/competitors/added_new_competitor" do
   c = Competitor.new
   c.name = params["add_competitor_name_textbox"]
+  age = params["competitor_age_dropdown"]
+  c.age = age.to_i
+  college = params["competitor_college_dropdown"]
+  c.college_id = college
   c.save
   erb :"/competitors/added_competitors_confirmation"
 end
