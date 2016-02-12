1acbd1d7163e5c19bcc0bf9984f9a464a52a4d0a
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-12 13:14:08 -0600

    Trying to include conference name in /schools page.

--- app/models/event_model.rb
+++ app/models/event_model.rb
@@ -5,14 +5,4 @@ class Event < ActiveRecord::Base
     y = results.where({"event_id" => x})
     return y
   end
-
-  def display_competitor_first_name(x)
-    y = Competitor.find_by_id(x)
-    return y.first_name
-  end
-
-  def display_competitor_last_name(x)
-    y = Competitor.find_by_id(x)
-    return y.last_name
-  end
 end
--- app/models/school_model.rb
+++ app/models/school_model.rb
@@ -1,2 +1,8 @@
 class School <ActiveRecord::Base
+  def display_conference_of_school
+    competitors = Competitor.all
+    x = self.school_name
+    y = competitors.where({"school_name" => x})
+    return y
+  end
 end
