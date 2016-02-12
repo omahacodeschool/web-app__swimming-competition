741f3e9209faf615351224952bce1c18d8600701
Kyle Sandage <ksandage08@gmail.com>
2016-02-12 14:46:04 -0600

    Stuck adding events to swimmer_bio

--- app/controllers/misc_controllers.rb
+++ app/controllers/misc_controllers.rb
@@ -2,6 +2,8 @@
 #events they are in
 MyApp.get "/swimmer_bio/:id" do
   @swimmer = Swimmer.find_by_id(params[:id])
+  @result = Result.where({@swimmer.swimmer_id => params[:id]})
+  binding.pry
   erb :"/bios/swimmer_bio"
 end
 
