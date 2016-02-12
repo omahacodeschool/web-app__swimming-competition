ca31defcbace26727d8e4646161f5269a468d03b
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-11 21:18:04 -0600

    get success pages, but don't think rows are being added to the database

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,11 +2,19 @@ MyApp.get "/create_event" do
   erb :"/create/create_event"
 end
 
+# shows the user they have successfully created a conference.
 MyApp.get "/create_event/success" do
   x = Event.new
-  x.name = params["event"]
-  # x.event = params["event"]
+  x.event_name = params["event_name"]
+  # this should be a new Active Record method that sets an Event Object attribute name to the information passed through params
   x.save
+  # this should be a new Active Record method that saves that Event Object to the "events" table
+  erb :"create/create_event_success"
+end
 
-  erb :"/create/create_event_success"
+# shows all conferences
+MyApp.get "/events" do
+  @events = Event.all
+  binding.pry
+  erb :"lists/all_events"
 end
