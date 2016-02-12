0bc7da1d5a1e40481eb37390e51905f1a794214a
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-12 13:11:07 -0600

    111

--- app/controllers/swimmingevents_controllers/finishes_controller.rb
+++ app/controllers/swimmingevents_controllers/finishes_controller.rb
@@ -1,4 +1,23 @@
 
+
+MyApp.get "/finishes" do
+  @events = Event.all
+  erb :"/ev/finishes"
+end
+
+MyApp.get "/view_event/:id" do
+  @currentevent = Event.find_by_id(params[:id])
+  @signups = Signup.all
+  z = Signup.where({"event_id" => (params[:id])})
+  @swimmers = []
+  z.each do |banana|
+    @swimmers << banana.find_swimmer_name
+  end
+  #binding.pry
+  erb :"/ev/view_event"
+end
+
+
 #finish refers to the TIME that the swimmer FINISHED a match
 #DB.define_table("finishes")
 #DB.define_column("finishes","signup_id","integer")
