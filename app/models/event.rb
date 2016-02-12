93b3c8aac76b3c636326c2044cbcfbab25365192
Michael Meier <mmeier61@yahoo.com>
2016-02-11 23:01:32 -0600

    Big Push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+  #return an array that displays all those in this event
+  def all_swimmers
+    x = self.id
+    return Swimmer.where({"event_id" => x})
+  end
 end
--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -1,2 +1,15 @@
 class Swimmer < ActiveRecord::Base
+  #returns all school information that I found relation to the school id
+  def school_info
+    x = self.school_id
+    y = School.find_by_id(x)
+    return y
+  end
+
+  #returns the school name when referencing the school id
+  def school_name
+  x = self.school_id
+  y = School.find_by_id(x)
+  return y.name
+end
 end
