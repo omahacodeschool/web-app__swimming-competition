03947b875f9b6fa24406ae0cac0351ba8c081dae
Chris Foral <chris.foral@gmail.com>
2016-02-12 00:43:37 -0600

    Simple(r) pages setup.

--- app/controllers/swimmers_controller.rb
+++ app/controllers/swimmers_controller.rb
@@ -1,7 +1,11 @@
-MyApp.get "swimmers_form" do
+MyApp.get "/swimmers_form" do
   @swimmers = Swimmer.all
-  @s.swimmer_name =
-  @s.
+  @s = Swimmer.new
+  @s.swimmer_name = (params["swimmer_to_add"])
+  @s.swimmer_dob = (params["swimmer_dob"])
+  @s.swimmer_gender = (params["swimmer_gender"])
+  @s.team_id = (params["select_team"])
+  @s.save
 
   erb :"main/add_swimmers"
 end
