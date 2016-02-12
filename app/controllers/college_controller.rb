0c5cd51eb7211b6f872883ce38d7e86e6b5e781c
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-12 14:58:05 -0600

    can update college rows / can update conference row

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -13,7 +13,22 @@ MyApp.get "/colleges/added_new_college" do
   erb :"/colleges/added_college_confirmation"
 end
 
+MyApp.post "/colleges/college/update_name/:id" do
+  @a = College.find_by_id(params[:id])
+  @a.name = params["update_college_name_textbox"]
+  @a.save
+  erb :"/colleges/update_college_confirmation"
+end
+
+MyApp.post "/colleges/college/updated_conference/:id" do
+  @c = College.find_by_id(params[:id])
+  @c.conference_id = params["update_college_conference_dropdown"]
+  @c.save
+  erb :"/colleges/update_college_confirmation"
+end
+
 MyApp.get "/colleges/college/:id" do
   @college = College.find_by_id(params[:id])
+  @conferences = Conference.all
   erb :"/colleges/college"
 end
--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -11,6 +11,13 @@ MyApp.get "/conferences/added_new_conference" do
   erb :"/conferences/added_conference_confirmation"
 end
 
+MyApp.post "/conferences/conference/update_name/:id" do
+  @c = Conference.find_by_id(params[:id])
+  @c.name = params["update_conference_name_textbox"]
+  @c.save
+  erb :"/conferences/update_conference_confirmation"
+end
+
 MyApp.get "/conferences/conference/:id" do
   @conference = Conference.find_by_id(params[:id])
   erb :"/conferences/conference"
