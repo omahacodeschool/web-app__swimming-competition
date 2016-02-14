4447cdd144101b0f9511647f5cb8fe5aa0379b35
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-13 23:29:14 -0600

    Phase 2 Part 1 complete

--- app/controllers/added_controller.rb
+++ app/controllers/added_controller.rb
@@ -1,30 +1,25 @@
 #Processes the form for adding an award
-#need something that ties to event 
-#rank refers to time in event details
 MyApp.get "/award_added" do 
   x = Award.new
-  x.name = params["award_name"]
+  x.rank = params["rank"]
   x.save
 
   erb :"added/award_added"
 end
 
 #Processes the form for adding a college
-#need something that ties to conf!!!
 MyApp.get "/college_added" do 
   x = College.new
-
-  x.name = params["college_name"]
+  x.college_name = params["college_name"]
   x.save
 
   erb :"added/college_added"
 end
 
 #Processes the form for adding a competitor
-#need something that ties to college!!!
 MyApp.get "/competitor_added" do 
   x = Competitor.new
-  x.name = params["competitor_name"]
+  x.competitor_name = params["competitor_name"]
   x.save
 
   erb :"added/competitor_added"
@@ -33,7 +28,7 @@ end
 #Processes the form for adding a conference
 MyApp.get "/conference_added" do 
   x = Conference.new
-  x.name = params["conference_name"]
+  x.conference_name = params["conference_name"]
   x.save
 
   erb :"added/conference_added"
@@ -42,17 +37,16 @@ end
 #Processes the form to add an event
 MyApp.get "/event_added" do 
   x = Event.new
-  x.name = params["event_name"]
+  x.event_name = params["event_name"]
   x.save
 
   erb :"added/event_added"
 end
 
 #Processes the form to add event details
-#need something that ties to event!!!
 MyApp.get "/event_detail_added" do 
   x = EventDetail.new
-  x.name = params["event_detail"]
+  x.finish_time = params["finish_time"]
   x.save
 
   erb :"added/event_detail_added"
