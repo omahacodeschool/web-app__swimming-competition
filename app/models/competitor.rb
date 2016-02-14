70ee0f70a849c760ef52c1172a30eb07fc4ef5c0
Thorin Butler <thorin.butler@gmail.com>
2016-02-14 14:30:19 -0600

    Managed to get it so that each individual competitor lists which school they attend, which conference that school is in, and which events they're competing in.  Not sure that all of that was strictly speaking necessary, but it was useful practice I think.  Anyhow, need to sort it so that each event lists who the top three finishers are...I think.  Taking a short break.

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -5,4 +5,16 @@ class Competitor < ActiveRecord::Base
     return @results
   end
 
+  def school_name
+    s = School.find_by_id(self.school_id)
+    school = s.school
+    return school
+  end
+
+  def conference_name
+    c = School.find_by_id(self.school_id)
+    conference = c.conference
+    return conference
+  end
+
 end
