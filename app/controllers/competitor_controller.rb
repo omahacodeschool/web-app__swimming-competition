40d024eb7c54ab9d2d92d064a91b99f32b8b4807
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-15 10:40:22 -0600

    method should be called 'school_id' per define_schema.rb

--- app/controllers/competitor_controller.rb
+++ app/controllers/competitor_controller.rb
@@ -9,7 +9,7 @@ MyApp.get "/create_competitor/success" do
   x = Competitor.new
   # x.competitor_name is a new Active Recored method that sets a Competitor Object attribute name to 'competitor_name'.
   x.competitor_name = params["competitor_entered"]
-  x.school_name = params["school"]
+  x.school_id = params["school"]
   # x.save is a new Active Record method that saves that Competitor Object to the 'competitors' table.
   x.save
   # binding.pry # what is 'x'?
