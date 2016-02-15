5c25b861e3f6420411e34db6ef9386ec286ad5de
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-15 15:09:37 -0600

    individual records have been added tothe chili was added, city was added, and competition was added views and are all working.

--- app/controllers/form_controllers.rb
+++ app/controllers/form_controllers.rb
@@ -1,26 +1,44 @@
 MyApp.get "/submit_chili" do
-
-erb :"chili_was_added"
+  @f = Chili.new
+  @f.name = params[:name_of_chili]
+  @f.contestant_id = params[:contestant_id]
+  @f.save
+  erb :"successful/chili_was_added"
 end
 
 MyApp.get "/submit_region" do
   @obj = Region.new
   @obj.city = params[:city_name]
   @obj.save
-erb :"successful/city_was_added"
+  erb :"successful/city_was_added"
 end
 
 MyApp.get "/submit_competition" do
-
-erb :"successful/competition_was_added"
+  @cat = Competition.new
+  @cat.name = params[:category]
+  @cat.save
+  erb :"successful/competition_was_added"
 end
 
 MyApp.get "/submit_contestant" do
-
-erb :"successful/contestant_was_added"
+  @c = Contestant.new
+  @c.first_name = params[:contestant_first_name]
+  @c.last_name = params[:contestant_last_name]
+  @c.region_id = params[:city_id]
+  @c.save
+  erb :"successful/contestant_was_added"
 end
 
 MyApp.get "submit_score" do
-
-erb :"successful/scores_was_added"
+  @score = Result.new
+  @score.chili_id = params[:chili_id]
+  @score.competition_id = params[:competition_id]
+  @score.aroma = params[:aroma]
+  @score.flavor = params[:flavor]
+  @score.texture = params[:texture]
+  @score.heat = params[:heat]
+  @score.overall = params[:overall]
+  @score.appearance = params[:appearance]
+  @score.save
+  erb :"successful/scores_was_added"
 end
--- app/controllers/view_all_controllers.rb
+++ app/controllers/view_all_controllers.rb
@@ -1,9 +1,4 @@
 MyApp.get "/view_contestants" do
-  @c = Contestant.new
-  @c.first_name = params[:contestant_first_name]
-  @c.last_name = params[:contestant_last_name]
-  @c.region_id = params[:city_id]
-  @c.save
   @con = Contestant.all
   erb :"all_entries/all_contestants"
 end
@@ -14,33 +9,16 @@ MyApp.get "/view_regions" do
 end
 
 MyApp.get "/view_chilies" do
-  @f = Chili.new
-  @f.name = params[:name_of_chili]
-  @f.contestant_id = params[:contestant_id]
-  @f.save
   @noms = Chili.all
   erb :"all_entries/all_chilies"
 end
 
 MyApp.get "/view_competitions" do
-  @cat = Competition.new
-  @cat.name = params[:category]
-  @cat.save
   @all_cats = Competition.all
   erb :"all_entries/all_competitions"
 end
 
 MyApp.get "/view_results" do
-  @score = Result.new
-  @score.chili_id = params[:chili_id]
-  @score.competition_id = params[:competition_id]
-  @score.aroma = params[:aroma]
-  @score.flavor = params[:flavor]
-  @score.texture = params[:texture]
-  @score.heat = params[:heat]
-  @score.overall = params[:overall]
-  @score.appearance = params[:appearance]
-  @score.save
   @scores = Result.all
   erb :"all_entries/all_results"
 end
