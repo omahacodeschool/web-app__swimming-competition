343046c2cd5558f6d0aea6156c52fc1e5d637f89
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-15 16:06:32 -0600

    can now view competitor school_name

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,11 +1,11 @@
 class Competitor < ActiveRecord::Base
 
-  # # get the school name
-  # #
-  # # returns school name String
-  # def school_name
-  #   x = self.school_id
-  #   y = School.find_by_id(x)
-  #   return y.school
-  # end
+  # gets the school_id from class Competitor
+  # returns school name String from class School
+  def school_name
+    x = self.school_id
+    y = School.find_by_id(x)
+    return y.school_name
+  end
+
 end
