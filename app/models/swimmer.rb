8d738c99bf92006b745eeae09cde8a72e0f8afc9
Patrick Clark <patclark1958@gmail.com>
2016-02-14 18:34:03 -0600

    Stumbled into a problem in race_results table that I think is about how the table standardizes formatting for time. To simplify, I was going to use Melinda's idea and record my time just as seconds and hundredths thereof, like 105.33 for 105 seconds and 33/100ths of a second. But even if I change the column from "time" to "float" in the schema, and change the column from "swimmer_time" to "finish" the table still wants its 00:00:00 format. So now I am entering some sample data that way and we'll see how it goes.

--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -1,9 +1,11 @@
 class Swimmer < ActiveRecord:: Base
 
 
-def finish_time(water)
- x = SwimEntry.find_by_swimmer_id_and_race_id(self.id, water)
- x.id
-
+def race_result(water)
+  x = SwimEntry.find_by_swimmer_id_and_race_id(self.id, water)
+  x.id
+  race_result = RaceResult.find_by_swim_entry_id(x.id)
+  return race_result
+  end
 
 end
