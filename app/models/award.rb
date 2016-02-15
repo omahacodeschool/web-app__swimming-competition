b10db9d5ed288c9120f78d55074a8b5b48e538d9
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-15 12:29:50 -0600

    Awards and Event Details view all pages are working now.

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,2 +1,16 @@
 class Award < ActiveRecord::Base
+  def event_name
+    if self.event_id == nil
+      return ""
+    else
+      Event.find_by_id(self.event_id).event_name
+    end
+  end
+  def competitor_name
+    if self.competitor_id == nil
+      return ""
+    else
+      Competitor.find_by_id(self.competitor_id).competitor_name
+    end
+  end
 end
