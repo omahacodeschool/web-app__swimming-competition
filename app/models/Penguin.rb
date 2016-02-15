c1f12e1165b46a0fc4e78fced2becf0350c4af3c
Ryan Newell <youcatastrophe@gmail.com>
2016-02-15 16:25:26 -0600

    Can't remember what I did last. Still committing anyway. In the middle of trying to find a way to delete all associated 'Results' when a Penguin is deleted.

--- app/models/Penguin.rb
+++ app/models/Penguin.rb
@@ -1,11 +1,20 @@
 class Penguin < ActiveRecord::Base
 
-  # Get the Penguin's Rookery info
-  #
-  #Returns the Rookery object.
   def rookery_access
     x = self.rookery_id
     y = Rookery.find_by_id(x)
     y
   end
+
+  def event_access
+    x = self.event_id
+    y = Event.find_by_id(x)
+    y
+  end
+
+  def all_results
+    x = Result.where({"penguin_id" => self.id})
+    x.id
+  end
+
 end
