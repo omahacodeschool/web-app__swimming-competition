255a61a29654f1068dd589eea9051a09ffb27a5d
Ryan Newell <youcatastrophe@gmail.com>
2016-02-15 13:43:55 -0600

    Successfully added delete buttons for all conference objects on all_conference.erb file.

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -26,12 +26,7 @@ MyApp.post "/conferences/process_edit/:id" do
   erb :"updated"
 end
 
-MyApp.get "/conferences/delete/:id" do
-  @conference = Conference.find_by_id(params[:id])
-  erb :"conferences/delete_conference"
-end
-
-MyApp.post "/conferences/process_delete/:id" do
+MyApp.post "/conferences/delete/:id" do
   @conference = Conference.find_by_id(params[:id])
   @conference.delete
   erb :"updated"
