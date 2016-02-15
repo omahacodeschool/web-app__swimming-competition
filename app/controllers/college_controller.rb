0de39fde714a22a437576ba148f4b81367cad79a
Patrick Clark <patclark1958@gmail.com>
2016-02-15 07:38:47 -0600

    Working on adding conference affiliations to college data. College table just has a conference ID, adding the conference name. The only data in the conference table is the conference_id and the conference name.
    Also added links from the main page to each race status page (which includes the race result if the race has been completed) instead of having admin enter the race number in a form, which was throwing an error message. There was enough real estate on the page for the links, and it solves the functionality problem of being able to see race results. And one less step for the admin.

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -46,3 +46,17 @@ MyApp.get "/college/create" do
 
   erb :"success_college_add"
 end
+
+MyApp.get "/college/view" do
+
+  # If a GET request is made to the root path, the following line of code
+  # looks for a .erb view file located in the 'views' directory at the given
+  # location. So it's going to look for views/main/welcome.erb.
+  # 
+  # Then it will combine that view file with the layout file and sent the
+  # combined document back to the client.
+
+  # Accepts the form entry data and creates the swimmer in the table. 
+  
+  erb :"college_status"
+end
--- app/models/college.rb
+++ app/models/college.rb
@@ -1,2 +1,23 @@
 class College < ActiveRecord:: Base
+
+# def conference_name
+#     confid = self.id #a conference_id for which we want to have conference name. There isn't anything else in the conference table.
+#     coll = Conference.where({"conference_id" => confid}) #anticipaing a hash where the key is the race_id and the value is the swim_entry_id
+#     g=[] #empty array to populate with swimmer ids for race "r"
+#     s.each do |swim_entry| #iterate through the swim_entry_ids
+#       x = swim_entry.swimmer_id #to get just the swimmer_ids for race "r" for use in querying the swimmer table. "x" is the array object with all of the swimmer ids.
+#       g.push(x) #push swimmer_ids "x" for race "r" into an Array
+#     end #end of each do loop
+
+#     return Swimmer.where({"id" => g})
+#     #return g   #array of swimmer ids for one race
+#   end #end of swimmer_ids_one_race method
+
+  def conference_name_with_college #name of this new method
+    conf = self.conference_id #conf is a collection of conference_ids in the College table that we will use to query the Conference database to get corresponding Conference names.
+    confnam = Conference.find_by_id(conf) #iterating through the conference ids to find the conference names. We should actually be returning all of the conference information, but the only information in the conference table is the conference name. 
+    return confnam #(Sumeet said it is ok to get the whole line of information)
+  end #end of swimmer_names_one_race method
+end #end of class SwimEntry
+
 end
