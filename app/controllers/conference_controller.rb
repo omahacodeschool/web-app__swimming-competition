99c71c13f8b4ae37ed72b828768be05b60b2cde6
Amy Wall <amymariewall@gmail.com>
2016-02-16 16:54:32 -0600

    All CRUD done, moving on to lock feature

--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -11,6 +11,30 @@ MyApp.post "/conference/create" do
   erb :"admin/confirm_submission"
 end
 
+MyApp.get "/update/conference/:conference_id" do
+  @conference = Conference.find_by_id(params[:conference_id])
+  erb :"admin/conference/update_conference"
+end
+
+MyApp.post "/process_update_conference_form/:conference_id" do
+  @conference = Conference.find_by_id(params[:conference_id])
+  @conference.conference_name = params["conference_name"]
+  @conference.save
+  @confirm_message = "Success! Updated #{@conference.conference_name}!"
+  erb :"admin/confirm_submission"
+end
+
+MyApp.post "/delete/conference/:conference_id" do
+  @conference = Conference.find_by_id(params[:conference_id])
+  if @conference.conference_empty? == true
+    @confirm_message = "Success! Deleted #{@conference.conference_name}!"
+    @conference.delete
+  else
+    @confirm_message = "There are Colleges in that Conference, so I will not delete it. Instead, go remove ALL Colleges from that Conference first."
+  end
+  erb :"admin/confirm_submission"
+end
+
 MyApp.get "/read/conferences" do
   @conferences = Conference.all
   erb :"admin/conference/read_conferences"
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,4 +1,19 @@
 # Conference Class goes here
 
 class Conference < ActiveRecord::Base
+
+  def conference_empty?
+    colleges = College.where("conference_id" => self.id)
+    if colleges == []
+      return true
+    else
+      return false
+    end
+  end
+
+
+
+
+
+
 end
