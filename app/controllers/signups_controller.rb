c42103364beba7c32cf73f9ca30df95d80e4d179
Jeremy Stoll <j8r8my@gmail.com>
2016-02-16 16:14:33 -0600

    Got all signup records to display in the view in a table, after re-working the methods

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,16 +1,16 @@
 MyApp.post "/view_add_signup_confirmation" do
 
-  @signup_1 = Signup.new
+  signup_new = Signup.new
 
-  @signup_1.swimmer_id = Swimmer.where("swimmer_name" => params["swim_signup_name"]).id
+  signup_new.swimmer_id = Swimmer.where("swimmer_name" => params["swim_signup_name"]).id
 
-  @signup_1.event_id = Event.where("event_name" => params["event_signup_name"]).id
-  @signup_1.save
+  signup_new.event_id = Event.where("event_name" => params["event_signup_name"]).id
+  signup_new.save
 
-  @show_got_event_id = @signup_1.id
-  @show_got_swim_id = @signup_1.swimmer_id
+  @show_got_event_id = signup_new.id
+  @show_got_swim_id = signup_new.swimmer_id
 
-  @all_signups = Signup.return_all_signups
+  @all_signups = Signup.all
 
   erb :"view_add_signup_confirmation"
 end
@@ -18,9 +18,7 @@ end
 
 MyApp.get "/view_add_signup" do
 
-binding.pry
-
-  @all_signups = Signup.return_all_signups
+  @all_signups = Signup.all
 
   
   erb :"view_add_signup"
--- app/models/Signup.rb
+++ app/models/Signup.rb
@@ -3,21 +3,33 @@ class Signup < ActiveRecord::Base
   def get_swimmer_name
     x = self.swimmer_id
     y = Swimmer.find_by_id(x)
-    return y.swimmer_name
+      if y.swimmer_name == nil
+        return "X"
+      else 
+        return y.swimmer_name
+      end
   end
 
   def get_school_name
     x = self.swimmer_id
     y = Swimmer.find_by_id(x)
-    z = y.school_id
+        z = y.school_id
     a = School.find_by_id(z)
-    return a.school_name
+      if a.school_name == nil
+        return "X"
+      else
+        return a.school_name
+    end
   end
 
   def get_event_name
     x = self.event_id
     y = Event.find_by_id(x)
-    return y.event_name
+      if y.event_name == nil
+        return "X"
+      else
+        return y.event_name
+      end
   end
 
   def get_conference_name
@@ -27,46 +39,11 @@ class Signup < ActiveRecord::Base
     a = School.find_by_id(z)
     e = a.conference_id
     f = Conference.find_by_id(e)
-    return f.conference_name
-  end
-  
-#returns an array of ordered arrays, that have been checked for nil values. algorithm sets each to an "X" or "Not Yet Scored" if nil.
-
-  def self.return_all_signups
-      @sorted_signup_records = []
-
-      r = self.all
-      r.each do |x|
-        @signup_records = []
-
-        primary_key = x.id
-        @signup_records << primary_key
-
-        name = x.get_swimmer_name
-          if name == nil
-            name = "X"
-          end
-        @signup_records << name
-
-        school = x.get_school_name
-          if school == nil
-            school = "X"
-          end
-        @signup_records << school
-   
-        event = x.get_event_name
-          if event == nil
-            event = "X"
-          end
-        @signup_records << event
-
-        time = x.completed_swim_time
-          if time == nil
-            time = "Not Yet Scored"
-          end
-        @signup_records << time
-        @sorted_signup_records << @signup_records
-        return @sorted_signup_records
+      if f == nil
+        return "X"
+      else
+        return f.conference_name
       end
-    end
+  end
+
 end
