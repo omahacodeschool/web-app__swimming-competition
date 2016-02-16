700343cd64d70022b35b086a073e62f1baa7d3d6
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-16 17:05:58 -0600

    fixed all bugs

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -8,24 +8,25 @@ class Competition < ActiveRecord::Base
     # an extraction of just the values (or cells) for the chili_id column from the ojects_by_competition_id collection
     chili_ids_collection = []
 
-    objects_by_competition_id.each do |row|
+    objects_by_competiton_id.each do |row|
       chili_ids_collection << row.chili_id
     end
 
     # objects_by_chili_id is a collection of rows in the chilis table with a matching value for chili_ids in the id column
-    objects_by_chili_id = Chili.where({"id" => chili_ids})
+    objects_by_chili_id = Chili.where({"id" => chili_ids_collection})
 
     # an extraction of just the values (or cells) for the contestant_id column from the objects_by_chili_id collection
-    contestant_ids = []
+    contestant_ids_collection = []
 
     objects_by_chili_id.each do |row|
-      contestant_ids << row.contestant_id
+      contestant_ids_collection << row.contestant_id
     end
 
     # objects_by_contestant_id is a collection of rows in the contestants table with a matching value for contestant_ids in the id column
-    objects_by_contestant_id = Contestant.where("id" => contestant_ids)
+    objects_by_contestant_id = Contestant.where("id" => contestant_ids_collection)
     
     #returns a collection of objects
     return objects_by_contestant_id
-
   end
+
+end
