e2d65f8927646c2b0263ea1b8c2d8dcf61ef8275
Amy Wall <amymariewall@gmail.com>
2016-02-16 14:57:07 -0600

    Added update college feature need to add link to view colleges page

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -13,6 +13,21 @@ MyApp.post "/college/create" do
   erb :"admin/confirm_submission"
 end
 
+MyApp.get "/update/college/:college_id" do
+  @college = College.find_by_id(params[:college_id])
+  @conferences = Conference.all
+  erb :"admin/college/update_college"
+end
+
+MyApp.post "/process_update_college_form/:college_id" do 
+  @college = College.find_by_id(params[:college_id])
+  @college.college_name = params["college_name"]
+  @college.conference_id = params["conference_id"]
+  @college.save
+  @confirm_message = "Success! Updated #{@college.college_name}!"
+  erb :"admin/confirm_submission"
+end
+
 MyApp.get "/read/colleges" do
   @colleges = College.all
   erb :"admin/college/read_colleges"
