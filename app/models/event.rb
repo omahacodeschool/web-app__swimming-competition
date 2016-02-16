08b4a1d873d29da8a3e61ccabfe57604db251119
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-16 17:42:52 -0600

    school names now display on single event page, don't  know exactly why, but they do.

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,17 @@ class Event < ActiveRecord::Base
     end
     return Competitor.where({"id" => comps})
   end
+
+  def school_name
+    x = self.school_id
+    y = School.find_by_id(x)
+    return y.school_name
+  end
+
+  def conf_name
+    x = self.conference_id
+    y = Conference.find_by_id(x)
+    return y.conference_name
+  end  
+  
 end
