e75e65b643638799a512f5ddc99f76640eb0694b
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-15 18:58:30 -0600

    OOOOOOH YEAAAAAAAH Pt 4 is done before...um...pt 3 is done. I'M A WILD CARD, GUYS.

--- app/controllers/swimmingevents_controllers/events_controller.rb
+++ app/controllers/swimmingevents_controllers/events_controller.rb
@@ -29,6 +29,18 @@ MyApp.get "/view_event/:id" do
   erb :"/ev/view_event"
 end
 
+MyApp.post "/delete_event/:id" do
+  @event = Event.find_by_id(params[:id])
+  x = Signup.where({"event_id" => (params[:id])})
+  binding.pry
+  if x == []
+    @event.delete
+    erb :"/cv/deletesuccess"
+  else 
+    erb :"/ev/deletefail"
+  end
+end
+
 
 
 #DB.define_table("events")
