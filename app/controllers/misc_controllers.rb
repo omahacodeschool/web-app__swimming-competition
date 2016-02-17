c3a69495d8f21e27f4bac6e86dbff9a821a22306
Kyle Sandage <ksandage08@gmail.com>
2016-02-17 13:16:26 -0600

    University bio works

--- app/controllers/misc_controllers.rb
+++ app/controllers/misc_controllers.rb
@@ -14,6 +14,14 @@ MyApp.get "/event_bio/:id" do
   erb :"/bios/event_bio"
 end
 
+# When an university is clicked, to to a view that shows all the swimmers who go to that university and their GPA. 
+MyApp.get "/university_bio/:id" do
+  @university = University.find_by_id(params[:id])
+  @swimmers = Swimmer.where({"university_id" => params[:id]})
+  #binding.pry
+  erb :"/bios/university_bio"
+end
+
 #Take to view showing list of events. Each event will be a link to a dynamic view that will show the fastes performers in that event. 
 MyApp.get "/show_standings" do
   @events = Event.all
