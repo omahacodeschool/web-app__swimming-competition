e53e78ed6478633b05318ede6d988f8a0761613c
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-17 13:12:22 -0600

    Added ability to see a competitor's college when viewing any of their individual pages. Tried to make conference show up as well (by using .where in Competitor model) but it's only returning "#", (just like when trying to view all associated colleges when viewing a single conference). Going to move on to something else for now.

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,2 +1,16 @@
 class Competitor < ActiveRecord::Base
+
+  # Get the conference name to display w/ single college
+  #
+  # Return conference name String
+  def coll_name
+    x = self.college_id
+    y = College.find_by_id(x)
+    return y.college_name
+  end
+
+  def conf_name
+    x = self.id
+    return Conference.where({"college_id" => x})
+  end
 end
