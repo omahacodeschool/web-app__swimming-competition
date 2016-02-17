992598a8581360b2d23f64feb946137c9191c073
Thorin Butler <thorin.butler@gmail.com>
2016-02-17 15:40:12 -0600

    We managed to get Phase II, Part III done.  It seems to be working rather well now.  We've already got Phase III done (I think) and part of Phase IV done.  That leaves, what's remaining of Phase IV and Phase V to complete.

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,14 @@ class Event < ActiveRecord::Base
     return results
   end
 
+  def completed
+    completed = Result.where("event_id" => self.id)
+    completed.each do |c|
+      if c.time == nil
+        return false
+      end
+    end
+    return true
+  end
+
 end
