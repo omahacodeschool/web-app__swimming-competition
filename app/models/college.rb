4576e2045452fc41150acec884a57d555de2191f
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-17 11:26:25 -0600

    Added ability to see conference when viewing one college. Tried to add ability to view all associated colleges when viewing one conference (by using .where in Conference model) but it's only returning "#" currently.

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,2 +1,11 @@
 class College < ActiveRecord::Base
+
+  # Get the conference name to display w/ single college
+  #
+  # Return conference name String
+  def conf_name
+    x = self.conference_id
+    y = Conference.find_by_id(x)
+    return y.conference_name
+  end
 end
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,2 +1,9 @@
 class Conference < ActiveRecord::Base
+
+  #Return an Array containing all of a conference's College objects
+
+  def conf_colleges
+    x = self.id
+    return College.where({"conference_id" => x})
+  end
 end
