3db09c4fac40e8f6aa3d07f9e5ad24e39d26bf9f
Thorin Butler <thorin.butler@gmail.com>
2016-02-17 10:21:00 -0600

    Now all the lists have tables and you can edit all of them.  Need to go back and sort out dependent data to delete.  Also, some tweaking in general is needed.

--- app/controllers/schools_controllers.rb
+++ app/controllers/schools_controllers.rb
@@ -1,7 +1,7 @@
 MyApp.post "/schools" do 
   x = School.new
   x.school = params["school"]
-  x.conference_id = params["conf_id"]
+  x.conference_id = params["conference_id"]
   x.save
   @y = [x.school, x.conference_id]
   erb :"schools/schools"
@@ -16,3 +16,29 @@ MyApp.get "/view_school/:school_id" do
   @school = School.find_by_id(params[:school_id])
   erb :"schools/single_school"
 end
+
+MyApp.post "/delete_school/:school_id" do
+  @school = School.find_by_id(params[:school_id])
+  @school.delete
+  @result = Result.where("event_id" => params[:event_id])
+  @result.each do |r|
+    r.delete
+  end
+  erb :"schools/deleted"
+end
+
+MyApp.get "/edit_school/:school_id" do
+  @school = School.find_by_id(params[:school_id])
+  erb :"schools/form_for_editing_school"
+end
+
+MyApp.post "/process_school_edit_form/:school_id" do
+  x = School.find_by_id(params[:school_id])
+  x.school = params["school"]
+  x.conference_id = params["conference_id"]
+  x.save
+  @y = [x.school, x.conference_id]
+  erb :"schools/processed"
+ end 
+
+
