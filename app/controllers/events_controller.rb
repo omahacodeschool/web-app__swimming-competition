1360fc84dfaba63704a6bc4d97276596d2ef00ac
Chris Foral <chris.foral@gmail.com>
2016-02-17 15:54:15 -0600

    Top 3 View is complete. The table fetaure on the page prints the swimmers in order of their run times. Now must have it only return the top 3 instead of all participants

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,26 +34,24 @@ MyApp.post "/process_event_update/:event_id" do
 
   erb :"main/add_events"
 end
-#
-# Takes Boolean result of my event_complete_form located in my add_runs View
-# For each event in my events table, the event_complete column is marked True
-# or False
-# Then the event_complete? method is called on each event
+
 MyApp.post "/event_complete_form/:event_id" do
   @events = Event.all
   @event =  Event.find_by_id(params[:event_id])
             
   @event.event_complete = (params["event_complete_check"])
   @event.save
+#
+# Method call below not needed for my 'Restrict access to Top 3' feature. May need to write code
+# to enable the lock feature.
   #@event.event_complete?
 
   erb :"main/add_runs"
 end
-#
-# Create route path for dynamic link to Top 3 page
+
 MyApp.get "/event_top_runs/:event_id" do
   @events = Event.all
   @event  = Event.find_by_id(params[:event_id])
 
-  erb :"main/event_top_runs/"
+  erb :"main/event_top_runs"
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,12 @@ class Event < ActiveRecord::Base
   def event_runs
     Run.where({"event_id" => self.id})
   end
+
+  def event_top_runs
+    qualifier = Run.where({"event_id" => self.id})
+
+    qualifier.order(:run_time)
+  end
 #
 # Define method that checks if event_complete is True or False for each event.
 # If True:
@@ -14,9 +20,9 @@ class Event < ActiveRecord::Base
 #   1. Keep link hidden
 #   2. Allow event run to be edited
   def event_complete?
-    events = Event.all
+    @events = Event.all
 
-    events.each do |event|
+    @events.each do |event|
       if event.event_complete
 # Maybe return a keyword that talks to the link on the view page telling it to
 # reveal the link?
