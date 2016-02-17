34eb300d028b9c47781c1069e2c43824e6524c98
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-17 05:40:37 -0600

    undefined method 'conference_name'

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -5,13 +5,21 @@ class Competitor < ActiveRecord::Base
   # gets 'schools.id' Integer from class School
   # returns 'school_name' String from class School
   def school_name
-    x = self.school_id
-    y = School.find_by_id(x)
-    return y.school_name
+    x = self.school_id # x = ("competitors.id", "competitor_name", "school_id")
+    y = School.find_by_id(x) # y = ("schools.id", "school_name", "conference_id")
+    return y.school_name # returns "school_name" of y
   end
 
   def conf_name
-    return "Conference Name"
+    # x = ("id", "school_name", "conference_id")
+    x = School.where({"id" => self.school_id})
+    comps = []
+    x.each do |c|
+      # pushes each "conference_id" into comps Array
+      comps << c.conference_id
+    end
+    # returns ("id", "conference_name")
+    return Conference.where({"id" => comps})
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,14 @@ class Event < ActiveRecord::Base
   # Return an Array containing all competitors for an event. 
   #(the Competitor Objects).
   def competitors
-    signups_for_this_event = Signup.where({"event_id" => self.id})
+    # x = ("signups.id", "competitor_id")
+    x = Signup.where({"event_id" => self.id})
     comps = []
-    signups_for_this_event.each do |e|
+    x.each do |e|
+      # pushes each "competitor_id" into comps Array
       comps << e.competitor_id
     end
+    # returns ("competitors.id", "competitor_name", "school_id")
     return Competitor.where({"id" => comps})
   end 
   
