f6f1453d64ff6d622374343c3badddd58632e0a2
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-19 14:42:57 -0600

    Result tests working

--- tests/unit/result_test.rb
+++ tests/unit/result_test.rb
@@ -14,22 +14,34 @@ class ResultTest < Minitest::Test
     @hermione_granger.last_name = "Granger"
     @hermione_granger.save
 
-    @hundred_m_race = Event.new
-    @hundred_m_race.gender = "female"
-    @hundred_m_race.save
+    @butterfly = Style.new
+    @butterfly.style = "butterfly"
+    @butterfly.save
 
-    @million_m_race = Event.new
-    @million_m_race.gender = "male"
-    @million_m_race.save
+    @backstroke = Style.new
+    @backstroke.style = "backstroke"
+    @backstroke.save
+
+    @hundred_m_butterfly = Event.new
+    @hundred_m_butterfly.gender = "female"
+    @hundred_m_butterfly.distance = 100
+    @hundred_m_butterfly.style_id = @butterfly.id
+    @hundred_m_butterfly.save
+
+    @million_m_backstroke = Event.new
+    @million_m_backstroke.gender = "male"
+    @million_m_backstroke.distance = 1000000
+    @million_m_backstroke.style_id = @backstroke.id
+    @million_m_backstroke.save
 
     @chads_result = Result.new
     @chads_result.competitor_id = @chad_leclaws.id
-    @chads_result.event_id = @hundred_m_race.id
+    @chads_result.event_id = @hundred_m_butterfly.id
     @chads_result.save
 
     @hermiones_result = Result.new
     @hermiones_result.competitor_id = @hermione_granger.id
-    @hermiones_result.event_id = @million_m_race.id
+    @hermiones_result.event_id = @million_m_backstroke.id
     @hermiones_result.save
 
   end
@@ -49,5 +61,15 @@ class ResultTest < Minitest::Test
     assert_equal(@hermiones_result.display_event_gender, "male")
     refute_equal(@chads_result.display_event_gender, "male")
     refute_equal(@hermiones_result.display_event_gender, "female")
+  end
+
+  def test_display_event_distance
+    assert_equal(@chads_result.display_event_distance, 100)
+    assert_equal(@hermiones_result.display_event_distance, 1000000)
+  end
+
+  def test_display_event_style
+    assert_equal(@chads_result.display_event_style, "butterfly")
+    assert_equal(@hermiones_result.display_event_style, "backstroke")
   end
 end
