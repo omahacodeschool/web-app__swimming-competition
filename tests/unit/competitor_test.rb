c13c388b4ba0ba7bcbdec90bfa9055c912c3ffd2
Thorin Butler <thorin.butler@gmail.com>
2016-02-19 13:48:44 -0600

    Got all the unit tests for the competitor model to pass.  It turned out to be a mixture of me doing a few foolish things and a some issues with ordering.  Foolish thing #1...I was trying to hard code IDs, which was an oversight and a bit of a misunderstanding on my part.  Mea culpa.  The other problem that was causing errors: I didn't order my setup correctly.  I was creating objects that had attributes that were set by other objects that hadn't been created yet.  So yeah, order is very important.  Now I just need to extend the lessons learned here to what remains.  Woohoo!

--- tests/unit/competitor_test.rb
+++ tests/unit/competitor_test.rb
@@ -3,58 +3,7 @@ require 'test_helper'
 class CompetitorTest < Minitest::Test
   def setup
     super
-
-    @competitor_1 = Competitor.new
-    @competitor_1.first_name = "Ben"
-    @competitor_1.last_name = "Grimm"
-    @competitor_1.school_id = 1
-    @competitor_1.save
-
-    @competitor_2 = Competitor.new
-    @competitor_2.first_name = "Susan"
-    @competitor_2.last_name = "Storm"
-    @competitor_2.school_id = 2
-    @competitor_2.save
-
-    @competitor_3 = Competitor.new
-    @competitor_3.first_name = "Reed"
-    @competitor_3.last_name = "Richards"
-    @competitor_3.school_id = 3
-    @competitor_3.save
-
-    @result_1 = Result.new
-    @result_1.event_id = 1
-    @result_1.competitor_id = 1
-    @result_1.time = 1
-    @result_1.save
-
-    @result_2 = Result.new
-    @result_2.event_id = 2
-    @result_2.competitor_id = 2
-    @result_2.time = 2
-    @result_2.save
-
-    @result_3 = Result.new
-    @result_3.event_id = 3
-    @result_3.competitor_id = 3
-    @result_3.time = 3
-    @result_3.save
-
-    @school_1 = School.new
-    @school_1.school = "Baxter"
-    @school_1.conference_id = 1
-    @school_1.save
-
-    @school_2 = School.new
-    @school_2.school = "Triskelion"
-    @school_2.conference_id = 2
-    @school_2.save
-
-    @school_3 = School.new
-    @school_3.school = "Xavier Academy"
-    @school_3.conference_id = 3
-    @school_3.save
-
+    
     @conference_1 = Conference.new
     @conference_1.conference = "Upper"
     @conference_1.save
@@ -66,6 +15,61 @@ class CompetitorTest < Minitest::Test
     @conference_3 = Conference.new
     @conference_3.conference = "Lower"
     @conference_3.save
+
+    @school_1 = School.new
+    @school_1.school = "Baxter"
+    @school_1.conference_id = @conference_1.id
+    @school_1.save
+
+    @school_2 = School.new
+    @school_2.school = "Triskelion"
+    @school_2.conference_id = @conference_2.id
+    @school_2.save
+
+    @school_3 = School.new
+    @school_3.school = "Xavier Academy"
+    @school_3.conference_id = @conference_3.id
+    @school_3.save
+
+    @competitor_1 = Competitor.new
+    @competitor_1.first_name = "Ben"
+    @competitor_1.last_name = "Grimm"
+    @competitor_1.school_id = @school_1.id
+    @competitor_1.save
+
+    @competitor_2 = Competitor.new
+    @competitor_2.first_name = "Susan"
+    @competitor_2.last_name = "Storm"
+    @competitor_2.school_id = @school_2.id
+    @competitor_2.save
+
+    @competitor_3 = Competitor.new
+    @competitor_3.first_name = "Reed"
+    @competitor_3.last_name = "Richards"
+    @competitor_3.school_id = @school_3.id
+    @competitor_3.save
+
+    @event = Event.new
+    @event.event = "walking"
+    @event.save
+
+    @result_1 = Result.new
+    @result_1.event_id = @event.id
+    @result_1.competitor_id = @competitor_1.id
+    @result_1.time = 1
+    @result_1.save
+
+    @result_2 = Result.new
+    @result_2.event_id = @event.id
+    @result_2.competitor_id = @competitor_2.id
+    @result_2.time = 2
+    @result_2.save
+
+    @result_3 = Result.new
+    @result_3.event_id = @event.id
+    @result_3.competitor_id = @competitor_3.id
+    @result_3.time = 3
+    @result_3.save
 
   end
 
@@ -91,10 +95,10 @@ class CompetitorTest < Minitest::Test
   end
 
   def test_conference_id
-    assert_equal(@competitor_1.conference_id, 1)
-    assert_equal(@competitor_2.conference_id, 2)
-    assert_equal(@competitor_3.conference_id, 3)
-    refute_equal(@competitor_1.conference_id, 3)
+    assert_equal(@competitor_1.conference_id, @conference_1.id)
+    assert_equal(@competitor_2.conference_id, @conference_2.id)
+    assert_equal(@competitor_3.conference_id, @conference_3.id)
+    refute_equal(@competitor_1.conference_id, @conference_3.id)
   end
 
 end
