75d13ff25112ca89f8e0068dabc0cd2f126d1501
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-20 15:25:58 -0600

    all update buttons take you to the update forms

---
+++ app/controllers/update_entry_controllers.rb
@@ -0,0 +1,23 @@
+MyApp.get "/update_city_form/:num" do
+  @city = Region.find_by_id(params[:num])
+  erb :"update_entry/update_city_form"
+end
+
+MyApp.get "/update_chili_form/:num" do
+  @chili = Chili.find_by_id(params[:num])
+  erb :"update_entry/update_chili_form"
+end
+
+MyApp.get "/update_competition_form/:num" do
+  @competition = Competition.find_by_id(params[:num])
+  erb :"update_entry/update_competition_form"
+end
+
+MyApp.get "/update_contestant_form/:num" do
+  @contestant = Contestant.find_by_id(params[:num])
+  erb :"update_entry/update_contestant_form"
+end
+MyApp.get "/update_scores_form/:num" do
+  @scores = result.find_by_id(params[:num])
+  erb :"update_entry/update_scores_form"
+end
