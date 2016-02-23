1d56c6e9782049006703dc7834665e392dcdea69
Jeremy Stoll <j8r8my@gmail.com>
2016-02-23 16:03:33 -0600

    Fixed checkboxes to edit time for signups.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,9 +1,12 @@
 MyApp.post "/add_score_confirmation" do
   @sorted_signup_records = Signup.sort_signups_for_viewing_table
-  x = Signup.find_by_id(params["signup_1_id"])
-  x.completed_swim_time =  params["signup_1_time"]
-  x.save
-
+  p = params["signup_ids_to_score"]
+  p.each do |i|
+    x = Signup.find_by_id(i)
+    x.completed_swim_time =  params["signup_1_time"]
+    x.save
+  end
+  @sorted_signup_records = Signup.sort_signups_for_viewing_table
 erb :"/scores/add_score"
 end
 
