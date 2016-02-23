18109f1b976f8cb44661c6da59e25117fe45bc4d
Jeremy Stoll <j8r8my@gmail.com>
2016-02-23 14:01:36 -0600

    Sign-ups are produced by drop-down menus, and fixed a Swimmer method where find_by_name was being used to use find_by_id.

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,32 +1,40 @@
 MyApp.post "/add_signup_confirmation" do
 
-  signup_new = Signup.new
+  @signups = Signup.all
 
-  swimmer_1 = Swimmer.find_by_swimmer_name(params["swim_signup_name"])
+  signup_new = Signup.new
+  @swimmers = Swimmer.all
+  @events = Event.all
+  swimmer_1 = Swimmer.find_by_id(params["swim_signup_id"])
   signup_new.swimmer_id = swimmer_1.id
 
-  x = Event.find_by_event_name(params["event_signup_name"])
+  x = Event.find_by_id(params["event_signup_id"])
   signup_new.event_id = x.id 
   signup_new.save
   
-  @all_signups = Signup.all
+  
 
   erb :"/signups/add_signup"
 end
 
 MyApp.post "/delete_signup/:signup_id" do
+  @signups = Signup.all
+  @swimmers = Swimmer.all
+  @events = Event.all
   signup_1 = Signup.find_by_id(params[:signup_id])
   signup_1.delete
 
-  @all_signups = Signup.all
+  
 
   erb :"/signups/add_signup"
 end
 
 
 MyApp.get "/add_signup" do
-
-  @all_signups = Signup.all
+  @signups = Signup.all
+  @swimmers = Swimmer.all
+  @events = Event.all
+  
 
   
   erb :"/signups/add_signup"
