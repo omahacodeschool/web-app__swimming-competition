6f1a6e68d2d7b05a8b093bb64dab600cc9182809
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-23 13:36:10 -0600

    making array to display various errors on one page--styles and schools models complete.

--- app/models/school_model.rb
+++ app/models/school_model.rb
@@ -11,12 +11,24 @@ class School <ActiveRecord::Base
     return lololol[0]
   end
 
-  def is_valid?
+  def set_errors
+    @errors = []
     if self.school_name == nil || self.school_name == ""
+      @errors << "School cannot be blank."
+    end
+  end
+
+  def is_valid?
+    self.set_errors
+    if @errors.length > 0
       return false
     else
       return true
     end
+  end
+
+  def get_errors
+    return @errors
   end
 
 end
--- app/models/style_model.rb
+++ app/models/style_model.rb
@@ -1,11 +1,23 @@
 class Style <ActiveRecord::Base
 
-  def is_valid?
+  def set_errors
+    @errors = []
     if self.style == nil || self.style == ""
+    @errors << "Style cannot be blank."
+    end
+  end
+
+  def is_valid?
+    self.set_errors
+    if @errors.length > 0
       return false
     else
       return true
     end
+  end
+
+  def get_errors
+    return @errors
   end
 
 end
