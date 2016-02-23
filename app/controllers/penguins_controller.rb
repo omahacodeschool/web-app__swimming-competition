697e2d28e8ef5b713a6f3b8f48f5d45c7075837e
Ryan Newell <youcatastrophe@gmail.com>
2016-02-23 12:36:11 -0600

    Validation methods added to Penguin model.

--- app/controllers/penguins_controller.rb
+++ app/controllers/penguins_controller.rb
@@ -4,14 +4,18 @@ MyApp.get "/penguins/new" do
 end
 
 MyApp.post "/penguins/create" do
-  x = Penguin.new
-  x.last_name = params["new_last_name"]
-  x.first_name = params["new_first_name"]
-  x.age = params["new_age"]
-  x.rookery_id = params["new_rookery_id"]
-  x.save
+  @penguin = Penguin.new
+  @penguin.last_name = params["new_last_name"]
+  @penguin.first_name = params["new_first_name"]
+  @penguin.age = params["new_age"]
+  @penguin.rookery_id = params["new_rookery_id"]
+  if @penguin.is_valid
+    @penguin.save
+    redirect "/penguins"
+  else 
+    erb :"penguins/error"
+  end
 
-  redirect "/penguins"
 end
 
 MyApp.get "/penguins" do
@@ -31,8 +35,14 @@ MyApp.post "/penguins/process_edit/:id" do
   @penguin.first_name = params["edit_first_name"]
   @penguin.age = params["edit_age"]
   @penguin.rookery_id = params["edit_rookery_id"]
-  @penguin.save
-  erb :"updated"
+
+  if @penguin.is_valid
+    @penguin.save
+    erb :"updated"
+  else 
+    erb :"penguins/error"
+  end
+
 end
 
 MyApp.post "/penguins/delete/:id" do
--- app/models/Penguin.rb
+++ app/models/Penguin.rb
@@ -1,5 +1,39 @@
 class Penguin < ActiveRecord::Base
 
+  def set_errors
+    @errors = []
+
+    if self.last_name == "" || self.last_name == nil
+      @errors << "Penguin's last name cannot be blank!"
+    end
+
+    if self.first_name == "" || self.first_name == nil
+      @errors << "Penguin's first name cannot be blank!"
+    end
+
+    if self.age == "" || self.age == nil
+      @errors << "Penguin's age cannot be blank!"
+    end
+
+    if self.rookery_id == "" || self.rookery_id == nil
+      @errors << "Rookery name cannot be blank!"
+    end    
+
+  end 
+
+  def get_errors
+    return @errors
+  end
+
+  def is_valid
+    self.set_errors
+    if @errors.length > 0
+      return false
+    else
+      return true
+    end
+  end   
+
   def rookery_access
     x = self.rookery_id
     y = Rookery.find_by_id(x)
