59fa3dd911e49a65b34960cf6e21378b035f09a6
Patrick Clark <patclark1958@gmail.com>
2016-02-23 17:27:01 -0600

    It's a glorious day, as I have added a dropdown menu to the form for adding a new swimmer. The dropdown lets them select the swimmer's college. All I really want to capture is the college id, which is the "value," but they see and select from the college names. I am happy and my swim meet, while still way far from presentable, is slightly more so. One thing has been picked up off the floor and put away.

--- app/controllers/swimmer_controller.rb
+++ app/controllers/swimmer_controller.rb
@@ -14,21 +14,11 @@ MyApp.post "/swimmer_delete/:id" do
   erb :"success_swimmer_deleted"
 end
 
-MyApp.get "/swimmer" do
 
-  # If a GET request is made to the root path, the following line of code
-  # looks for a .erb view file located in the 'views' directory at the given
-  # location. So it's going to look for views/main/welcome.erb.
-  # 
-  # Then it will combine that view file with the layout file and sent the
-  # combined document back to the client.
-
-  # DO I NEED TO DISPLAY THE WHOLE TABLE FOR THE ADMIN TO VERIFY THEIR SWIMMER ADD? 
-  
-  erb :"new_swimmer"
-end
 
 MyApp.get "/swimmer/new" do
+
+  @colleges = College.all
 
   # If a GET request is made to the root path, the following line of code
   # looks for a .erb view file located in the 'views' directory at the given
